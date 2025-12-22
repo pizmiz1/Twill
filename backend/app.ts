c6e2f897a8e7c6c061d9d990df2fdd146330e52a
pizmiz1 <ebritton12321@gmail.com>
2025-12-22 15:27:26 -0500

    Base endpoint returns json

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
-import express from "express";
+import express, { Response } from "express";
 import { requestLogger } from "./api/middleware/logger.js";
 import mongoose from "mongoose";
 import authRoutes from "./api/routes/authRoutes.js";
 import cors from "cors";
 import moduleRoutes from "./api/routes/moduleRoutes.js";
+import { JsonDto } from "../shared/jsondto.js";
 
 console.log("Starting Server");
 
@@ -22,8 +23,8 @@ app.use(requestLogger);
 // Routes
 app.use("/", authRoutes);
 app.use("/", moduleRoutes);
-app.get("/", (req, res) => {
-  res.status(201).send("Twill server is healthy.");
+app.get("/", (req, res: Response<JsonDto<string>>) => {
+  res.status(201).json({ data: "Twill server is healthy." });
 });
 
 // Run
